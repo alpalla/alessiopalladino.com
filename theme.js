b71df17a187bd0c2e56962bd569c86b4dee47838
alpalla <46252231+alpalla@users.noreply.github.com>
2024-11-11 19:45:12 +0000

    Automated release

---
+++ theme.js
@@ -0,0 +1,37 @@
+;(function initTheme() {
+  const storedTheme = localStorage.getItem("theme")
+
+  const applyTheme = (theme) => {
+    document.documentElement.setAttribute("data-theme", theme)
+
+    const disabledTheme = theme === "dark" ? "light" : "dark"
+
+    document
+      .getElementById(`${disabledTheme}-css`)
+      ?.setAttribute("disabled", "true")
+
+    document.getElementById(`${theme}-css`)?.removeAttribute("disabled")
+  }
+
+  if (storedTheme) {
+    applyTheme(storedTheme)
+
+    return
+  }
+
+  window
+    .matchMedia(`(prefers-color-scheme: dark)`)
+    .addEventListener("change", ({ matches }) => {
+      if (localStorage.getItem("theme")) {
+        return
+      }
+
+      applyTheme(matches ? "dark" : "light")
+    })
+
+  const prefersDarkColorScheme = matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches
+
+  prefersDarkColorScheme && applyTheme("dark")
+})()
