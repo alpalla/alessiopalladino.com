f396171612a36696d0d43ad60e1b4f20a8b15c1b
alpalla <46252231+alpalla@users.noreply.github.com>
2024-12-07 09:18:50 +0000

    Automated release

--- _next/static/chunks/app/about/page-2342a01af9670f06.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,352,831],{1281:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4839,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[839,441,517,358],()=>s(1281)),_N_E=e.O()}]);
