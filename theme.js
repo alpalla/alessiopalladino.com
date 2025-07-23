dcdd41f2cb1b43451c71e5c6bba36c9ca29405fc
alpalla <46252231+alpalla@users.noreply.github.com>
2025-07-23 09:59:41 +0000

    Automated release

--- theme.js
+++ theme.js
@@ -1,37 +1,37 @@
 ;(function initTheme() {
-  const storedTheme = localStorage.getItem("theme")
+	const storedTheme = localStorage.getItem("theme")
 
-  const applyTheme = (theme) => {
-    document.documentElement.setAttribute("data-theme", theme)
+	const applyTheme = (theme) => {
+		document.documentElement.setAttribute("data-theme", theme)
 
-    const disabledTheme = theme === "dark" ? "light" : "dark"
+		const disabledTheme = theme === "dark" ? "light" : "dark"
 
-    document
-      .getElementById(`${disabledTheme}-css`)
-      ?.setAttribute("disabled", "true")
+		document
+			.getElementById(`${disabledTheme}-css`)
+			?.setAttribute("disabled", "true")
 
-    document.getElementById(`${theme}-css`)?.removeAttribute("disabled")
-  }
+		document.getElementById(`${theme}-css`)?.removeAttribute("disabled")
+	}
 
-  if (storedTheme) {
-    applyTheme(storedTheme)
+	if (storedTheme) {
+		applyTheme(storedTheme)
 
-    return
-  }
+		return
+	}
 
-  window
-    .matchMedia(`(prefers-color-scheme: dark)`)
-    .addEventListener("change", ({ matches }) => {
-      if (localStorage.getItem("theme")) {
-        return
-      }
+	window
+		.matchMedia(`(prefers-color-scheme: dark)`)
+		.addEventListener("change", ({ matches }) => {
+			if (localStorage.getItem("theme")) {
+				return
+			}
 
-      applyTheme(matches ? "dark" : "light")
-    })
+			applyTheme(matches ? "dark" : "light")
+		})
 
-  const prefersDarkColorScheme = matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches
+	const prefersDarkColorScheme = matchMedia(
+		"(prefers-color-scheme: dark)",
+	).matches
 
-  prefersDarkColorScheme && applyTheme("dark")
+	prefersDarkColorScheme && applyTheme("dark")
 })()
